2a6f581dbec4ca36d23f47dbecd7cd5278ddb22b
WangBangQi <1597176979@qq.com>
2019-08-15 14:46:20 +0800

    soap

---
+++ src/main/java/cn/zimo/wbq/utils/soap/SoapRequest.java
@@ -0,0 +1,12 @@
+package cn.zimo.wbq.utils.soap;
+
+/**
+ * @author zi.mo
+ * @description
+ * @createTime 2019/8/15,1:45 PM
+ */
+public class SoapRequest {
+
+
+
+}
---
+++ src/main/java/cn/zimo/wbq/utils/soap/SoapResponse.java
@@ -0,0 +1,9 @@
+package cn.zimo.wbq.utils.soap;
+
+/**
+ * @author zi.mo
+ * @description
+ * @createTime 2019/8/15,1:46 PM
+ */
+public class SoapResponse {
+}
